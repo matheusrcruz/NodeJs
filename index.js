6069d92b88a48f10e8a51c4e8df810ba80a80b9d
Matheus Rufino Cruz <mrcruz1@stefanini.com>
2021-10-17 12:44:22 -0300

    ADD: refactor criando um script para executar lib

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
-function extraiLinks(texto){
+function extraiLinks(texto) {
     const regex = /\[([^\]]*)\]\((https:?\/\/[^$#\s].[^\s]*)\)/gm;
     const arrayResult = [];
     let temp;
-    while((temp = regex.exec(texto)) !== null )
-    {
-        arrayResult.push({ [temp[1]]: temp[2] })
+    while ((temp = regex.exec(texto)) !== null) {
+        arrayResult.push({
+            [temp[1]]: temp[2]
+        })
     }
-  return arrayResult;
+    return arrayResult.length === 0 ? 'não ha arquivos': arrayResult;
 }
 
-function tratarError(erro){
+function tratarError(erro) {
     throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho'));
 }
 
-async function pegaArquivo(caminhoArquivo){
+async function pegaArquivo(caminhoArquivo) {
     const encoding = 'utf-8';
 
-    try{
-    const texto = await fs.promises.readFile(caminhoArquivo, encoding);
-    console.log(extraiLinks(texto));
-    }
-    catch(erro){
-    tratarError(erro);
-    }
-    finally {
+    try {
+        const texto = await fs.promises.readFile(caminhoArquivo, encoding);
+        return extraiLinks(texto);
+    } catch (erro) {
+        tratarError(erro);
+    } finally {
         console.log(chalk.yellow('operação concluída'));
     }
 }
-pegaArquivo('./arquivos/texto1.md');
 
-
-
-
-
+module.exports = pegaArquivo;
