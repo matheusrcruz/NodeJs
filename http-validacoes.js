59aad322969ba452a0803814d5eba3fd09479c88
Matheus Rufino Cruz <mrcruz1@stefanini.com>
2021-10-24 20:12:18 -0300

    ADD: validated urls and return status code

--- http-validacoes.js
+++ http-validacoes.js
@@ -1,12 +1,41 @@
-    function geraArrayURLs(arrayLinks){
-        return arrayLinks.map(objtolink => Object.values(objtolink).join());
-    // return arrayLinks.map(objetoLink => Object.values(objetoLink).join());
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+function manejaErros(erro){
+    throw new Error(erro.message);
+}
+
+async function checaStatus(arrayURLs){
+    //promises async await
+    try{
+    const arrayStatus = await Promise
+    .all(arrayURLs
+        .map(async url =>{
+            const res = await fetch(url)
+            return res.status;
+    }))
+    return arrayStatus;
+    }catch(erro){
+        manejaErros(erro);
     }
+  
+}
 
+function geraArrayURLs(arrayLinks) {
+    return arrayLinks
+    .map(objtolink => Object
+        .values(objtolink).join());
+}
 
-    function validaURLs(arrayLinks){
-        return geraArrayURLs(arrayLinks);
-        // return geraArrayURLs(arrayLinks);
-    }
+//spread operator
+async function validaURLs(arrayLinks) {
+  const links = geraArrayURLs(arrayLinks);
+  const statusLinks = await checaStatus(links);
+  const resultados = arrayLinks.map((objeto, indice) =>/*Paratenses para envolver o objeto*/ ({ 
+      ...objeto, 
+      status: statusLinks[indice]
+    }))
 
-    module.exports = validaURLs;
+    return resultados
+}
+
+module.exports = validaURLs;
